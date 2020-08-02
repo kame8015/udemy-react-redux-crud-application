1d2c5855e2261e3451158026e784af3b9b0b10d8
kame8015 <kame8015@gmail.com>
2020-08-02 21:02:41 +0900

    implement store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux' // storeを作成するパッケージ
+import { Provider } from 'react-redux' // 作成したstoreを全コンポーネントに渡すproviderのパッケージ
+
 import './index.css';
-import App from './App';
+import reducer from './reducers'
+
+import App from '../components/App';
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(reducer)
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>
   document.getElementById('root')
 );
 
