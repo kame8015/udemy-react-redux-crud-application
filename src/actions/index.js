eb44764ee27ce2db391fb2e3f56af196b5af20cb
kame8015 <kame8015@gmail.com>
2020-08-02 20:43:47 +0900

    implement action and action creator.

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+
+// actionクリエイター
+export const incremetnt = () => ({
+	type: INCREMENT
+})
+
+// actionクリエイター
+export const decremetnt = () => ({
+	type: DECREMENT
+})
